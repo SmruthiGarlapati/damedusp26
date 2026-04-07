cfaf1cc55732bfeaca5ed6c25348c851152fb9f0
PanavMhatre <mhatrepanav@gmail.com>
2026-04-06 19:59:33 -0500

    Fix navbar hydration state

--- rapt/src/app/schedule/page.tsx
+++ rapt/src/app/schedule/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useCallback, useEffect, Suspense } from "react";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import Button from "@/components/Button";
