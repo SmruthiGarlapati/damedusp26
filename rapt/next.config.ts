d23dc8f362e3ea3ae408328d6843e1d77960b1e7
PanavMhatre <mhatrepanav@gmail.com>
2026-04-14 17:27:51 -0500

    updated next config

--- rapt/next.config.ts
+++ rapt/next.config.ts
@@ -18,6 +18,7 @@ const nextConfig: NextConfig = {
   // Next 16 blocks internal dev routing assets when the app is opened from
   // 127.0.0.1 or an IDE/LAN preview host unless we allow those origins.
   allowedDevOrigins: getAllowedDevOrigins(),
+  devIndicators: false,
 };
 
 export default nextConfig;
